b31fca512e3933f372cba727c83e2b55072bec8b
Weronika Skakovska <skakovskaweronika@gmail.com>
2025-10-26 21:49:10 +0100

    Features 2-5

--- src/account.py
+++ src/account.py
@@ -1,5 +1,41 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
+
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = "Invalid"
+
         self.balance = 0
+
+        if self._promo_code_validation(promo_code) and self._age_validation():
+            self.balance += 50
+
+    def _promo_code_validation(self, promo_code):
+        if promo_code is None:
+            return False
+        else:
+            return promo_code.startswith("PROM_")
+
+    def _age_validation(self):
+        if self.pesel == "Invalid":
+            return False
+
+        year = int(self.pesel[0:2])
+        month = int(self.pesel[2:4])
+
+        if 1 <= month <= 12:
+            year += 1900
+        elif 21 <= month <=32:
+            year += 2000
+        elif 41 <= month <=52:
+            year += 2100
+        elif 61 <= month <=72:
+            year += 2200
+        elif 81 <= month <=92:
+            year += 1800
+        else:
+            return False
+        return year > 1960
--- tests/unit/test_account.py
+++ tests/unit/test_account.py
@@ -3,7 +3,36 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "12345678910")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0
+        assert account.pesel == "12345678910"
+
+    def test_pesel_too_short(self):
+        account = Account("Jane", "Doe", "12345")
+        assert account.pesel == "Invalid"
+
+    def test_pesel_too_long(self):
+        account = Account("Jane", "Doe", "12345678901011")
+        assert account.pesel == "Invalid"
+
+    def test_promo_code(self):
+        account = Account("Jan", "Kowalski", "02270803628", promo_code = "PROM_ABC")
+        assert account.balance == 50
+
+    def test_wrong_promo_code(self):
+        account = Account("Jan", "Kowalski", "12345678910", promo_code = "CODE_ABC")
+        assert account.balance == 0
+
+    def test_no_promo_code(self):
+        account = Account("Jan", "Kowalski", "12345678910")
+        assert account.balance == 0
+
+    def test_correct_age_promo_code(self):
+        account = Account("Jan", "Kowalski", "02270803628", promo_code = "PROM_ABC")
+        assert account.balance == 50
+
+    def test_wrong_age_promo_code(self):
+        account = Account("Jan", "Kowalski", "59270803628", promo_code = "CODE_ABC")
         assert account.balance == 0
